f41b52d74d6ba3de9f1a31fdb2d4d6a40b9fb9b8
maalin2 <151789225+maalin2@users.noreply.github.com>
2026-08-19 23:34:21 -0400

    cuda.is_available

--- src/cuda_hello.py
+++ src/cuda_hello.py
@@ -2,9 +2,9 @@ import torch
 
 def main():
     print("pytorch: ", torch.__version__)
-    print("cuda available: ", torch.cuda_is_available())
+    print("cuda available: ", torch.cuda.is_available())
 
-    if torch.cuda_is_available():
+    if torch.cuda.is_available():
         print("gpu:", torch.cuda.get_device_name(0))
 
     x = torch.tensor([1,2,3])
