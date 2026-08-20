6246666306dec8279ca8b50a09f6177612155660
maalin2 <151789225+maalin2@users.noreply.github.com>
2026-08-19 23:26:40 -0400

    cuda hello

---
+++ src/cuda_hello.py
@@ -0,0 +1,13 @@
+import torch
+
+def main():
+    print("pytorch: ", torch.__version__)
+    print("cuda available: ", torch.cuda_is_available())
+
+    if torch.cuda_is_available():
+        print("gpu:", torch.cuda.get_device_name(0))
+
+    x = torch.tensor([1,2,3])
+    y = torch.tensor([10,20,30])
+
+    print("cpu result: ", x + y)
